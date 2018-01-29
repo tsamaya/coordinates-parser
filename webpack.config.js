1e2d129b4345cf9b7a7b6f1d50d205f7a10a6d0e
arnaud ferrand <arferrand@gmail.com>
2018-01-29 21:03:52 +0000

    add a browser lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const webpack = require("webpack");
+const path = require('path');
+
+const nodeConfig = {
   entry: './src/index.js',
   target: 'node',
   module: {
@@ -12,5 +15,34 @@ module.exports = {
   output: {
     libraryTarget: 'commonjs',
     filename: './index.js'
-  },
+  }
 };
+
+const webConfig = {
+  entry: {
+    'coordinatesparser': './src/index.js',
+    'coordinatesparser.min': './src/index.js'
+  },
+  devtool: "source-map",
+  target: 'web', // <=== can be omitted as default is 'web'
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ]
+};
+
+module.exports = [nodeConfig, webConfig];
